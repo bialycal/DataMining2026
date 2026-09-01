cb50bf0dd068782829a31d4a147a2cdf09d94a75
Bialy <bcalderonm@gmail.com>
2026-08-31 19:52:14 -0600

    imput_missing completo

--- mintic/eda/imput_missing.py
+++ mintic/eda/imput_missing.py
@@ -1,15 +1,22 @@
 #Recibe un DataFrame y una lista opcional de nombres de columnas, y debe imputar los valores faltantes según corresponda.
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-def impute_missing(df, columns = None):
+def impute_missing(df, strategy = "mean", columns = None):
+    df2= df.copy()
     vacios = []
-    columnas = list(df.keys())
+    columnas = list(df2.keys())
     for c in columnas:
-        if df[c].isnull().any():
+        if df2[c].isnull().any():
             vacios.append(c)
+            
+    if len(vacios) == 0:
+        return df2
 
-        if len(vacios) == 0:
-            return df
-        #else: pendiente
+    else:
+        for v in vacios:
+            if strategy == "mean":
+                df2[v] = df2[v].fillna(df2[v].mean())
+            elif strategy == "median":
+                df2[v] = df2[v].fillna(df2[v].median())
+            elif strategy == "mode":
+                df2[v] = df2[v].fillna(df2[v].mode()[0])
+        return df2

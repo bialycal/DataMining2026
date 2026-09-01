4e2831afb256f75d1e566ac75771d4b39ef60a06
Bialy <bcalderonm@gmail.com>
2026-09-01 13:07:50 -0600

    correccion imput_missing (sustitución de mode, mean y median por funciones propias y no de pandas)

--- mintic/eda/imput_missing.py
+++ mintic/eda/imput_missing.py
@@ -1,5 +1,19 @@
 #Recibe un DataFrame y una lista opcional de nombres de columnas, y debe imputar los valores faltantes según corresponda.
 
+def mean(lista):
+    return sum(lista) / len(lista)
+
+def median(lista):
+    lista.sort()
+    if len(lista) %2 == 1:
+        return lista[len(lista)//2]
+    else:
+        return (lista[len(lista)//2 - 1] + lista [len(lista)//2]) / 2
+
+def mode(lista):
+    moda  = pd.DataFrame(lista).value_counts()
+    return moda.index[0][0]
+
 def impute_missing(df, strategy = "mean", columns = None):
     df2= df.copy()
     vacios = []
@@ -14,9 +28,9 @@ def impute_missing(df, strategy = "mean", columns = None):
     else:
         for v in vacios:
             if strategy == "mean":
-                df2[v] = df2[v].fillna(df2[v].mean())
+                df2[v] = df2[v].fillna(mean(df2[v]))
             elif strategy == "median":
-                df2[v] = df2[v].fillna(df2[v].median())
+                df2[v] = df2[v].fillna(median(df2[v]))
             elif strategy == "mode":
-                df2[v] = df2[v].fillna(df2[v].mode()[0])
+                df2[v] = df2[v].fillna(mode(df2[v]))
         return df2

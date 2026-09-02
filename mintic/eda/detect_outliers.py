2a41bd64dc1cba09a468df0643c87282aea1e3ae
Bialy <bcalderonm@gmail.com>
2026-09-02 13:21:31 -0600

    detect_outliers completo

--- mintic/eda/detect_outliers.py
+++ mintic/eda/detect_outliers.py
@@ -8,15 +8,15 @@ def detect_outliers(df, method = 'iqr', umbral = 1.5):
         if pd.api.types.is_numeric_dtype(df2[c]):
             num.append(c)
 
-    for n in num:
-        quart = df2[n].quantile([0.25, 0.75])
-        iqr = quart[0.75] - quart[0.25]
-        lim_inf = quart[0.25] - umbral * iqr
-        lim_sup = quart[0.75] + umbral * iqr
-        
-        for m in df2[n]:
-            if (m < lim_inf) or (m > lim_sup):
-                #m = True
-            else:
-                #m = False
+    if method == 'iqr':
+        for n in num:
+            quart = df2[n].quantile([0.25, 0.75])
+            iqr = quart[0.75] - quart[0.25]
+            lim_inf = quart[0.25] - umbral * iqr
+            lim_sup = quart[0.75] + umbral * iqr
+
+            outliers_abajo = df2[n] < lim_inf 
+            outliers_arriba = df2[n] > lim_sup
+            df2[n] = outliers_abajo | outliers_arriba
+            
     return df2

80d05c741990b5137c5bd823f35ddbaae344ec3b
Bialy <bcalderonm@gmail.com>
2026-09-02 13:44:14 -0600

    correciones detect_outliers (zscore)

--- mintic/eda/detect_outliers.py
+++ mintic/eda/detect_outliers.py
@@ -1,5 +1,20 @@
 #Identifica la presencia de valores atípicos en el DataFrame basándose en un método estadístico y un umbral especificado.
 
+import pandas as pd
+import numpy as np
+
+def mean(lista):
+    return sum(lista) / len(lista)
+
+def sd(lista):
+    suma = 0
+
+    for x in lista:
+        suma += (x - mean(lista)) ** 2
+
+    return np.sqrt(suma / len(lista))
+
+    
 def detect_outliers(df, method = 'iqr', umbral = 1.5):
     df2 = df.copy()
     
@@ -18,5 +33,11 @@ def detect_outliers(df, method = 'iqr', umbral = 1.5):
             outliers_abajo = df2[n] < lim_inf 
             outliers_arriba = df2[n] > lim_sup
             df2[n] = outliers_abajo | outliers_arriba
+
+    elif method == 'zscore':
+        for n in num:
+            outliers_arriba = (df2[n] - mean(df2[n])) / sd(df2[n]) > 3
+            outliers_abajo = (df2[n] - mean(df2[n])) / sd(df2[n]) < -3
+            df2[n] = outliers_arriba | outliers_abajo
             
     return df2
